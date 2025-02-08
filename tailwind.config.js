2ea502c9edfcebbe7762fc5a9d5ad9eccf4045d1
Louis Chau <louis321yh@gmail.com>
2025-02-09 00:55:39 +0800

    Added page for client-server API

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
     purge: [],
     theme: {
-      extend: {},
+      extend: {
+        maxHeight: {
+          '1/2': '50%',
+          '50vh': '50vh',
+          '30vh': '30vh',
+          'full': '100%'
+        },
+        maxWidth: {
+          '1/2': '50%',
+          'full': '100%',
+          'none': 'none',
+        },
+        width: {
+          'auto': 'auto',
+        },
+        padding: {
+          '5': '1.25rem',
+        },
+        display: ['hover', 'focus'],
+      },
     },
     variants: {},
     plugins: [
